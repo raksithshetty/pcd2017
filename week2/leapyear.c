f56ab8290d9d7eeff44c31a1e2051e73cbc4aa65
raksithshetty <shettyrakshith37@gmail.com>
2017-03-08 14:55:40 +0530

    leap year

---
+++ week2/leapyear.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int isleap(int yr)
+{
+if(yr%400==00)
+{
+return 0;
+}
+else if(yr%100==0)
+{
+return 1;
+}
+else if(yr%4==0)
+{
+return 2;
+}
+else 
+{
+return -1;
+}
+}
+int main()
+{ 
+int year,r;
+printf("enter the year to be checked for leap year\n");
+scanf("%d",&year);
+r=isleap(year);
+switch(r)
+{
+case 0:
+printf("it is an quadranal leap year\n");
+break;
+case 1:
+printf("it is an century leap year\n");
+break;
+case 2:
+printf("it is an leap year\n");
+break;
